efb9ac56bcd54020738abab8753a3f5ece85cdb0
Namraj Pudasaini <active99raj@gmail.com>
2022-09-07 17:42:50 +1000

    feat: add netlify.toml file for Redirect and rewrite rule

---
+++ src/pages/ContactPage.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "../index.scss";
+
+const ContactPage = () => (
+  <section className="container">
+    <h1> This is my Contact Page. </h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident,
+      deserunt.Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Provident, deserunt.Lorem ipsum dolor sit amet, consectetur adipisicing
+      elit. Provident, deserunt.Lorem ipsum dolor sit amet, consectetur
+      adipisicing elit. Provident, deserunt.Lorem ipsum dolor sit amet,
+      consectetur adipisicing elit. Provident, deserunt.
+    </p>
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident,
+      deserunt.Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Provident, deserunt.Lorem ipsum dolor sit amet, consectetur adipisicing
+      elit. Provident, deserunt.Lorem ipsum dolor sit amet, consectetur
+      adipisicing elit. Provident, deserunt.
+    </p>
+  </section>
+);
+
+export default ContactPage;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "../index.scss";
+
+const HomePage = () => {
+  return (
+    <section className="container">
+      <h1> This is my Home Page. </h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident,
+        deserunt.Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Provident, deserunt.Lorem ipsum dolor sit amet, consectetur adipisicing
+        elit. Provident, deserunt.Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Provident, deserunt.Lorem ipsum dolor sit amet,
+        consectetur adipisicing elit. Provident, deserunt.
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident,
+        deserunt.Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Provident, deserunt.Lorem ipsum dolor sit amet, consectetur adipisicing
+        elit. Provident, deserunt.Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Provident, deserunt.
+      </p>
+    </section>
+  );
+};
+
+export default HomePage;
